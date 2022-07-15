e270d1d57ad19e7c77c62446d034587f65d2034c
Scottie Enriquez <scottie@enriquez.xxx>
2022-07-15 01:24:24 -0700

    Working pull request flow

--- infracost-cdk-pipeline/lib/infracost-cdk-pipeline-stack.ts
+++ infracost-cdk-pipeline/lib/infracost-cdk-pipeline-stack.ts
@@ -5,14 +5,17 @@ import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as events from 'aws-cdk-lib/aws-events';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import * as uuid from 'uuid';
 
 export class InfracostCdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
     // constants
     const terraformVersion = '1.2.4';
     const mainBranchName = 'main';
@@ -24,12 +27,15 @@ export class InfracostCdkPipelineStack extends Stack {
       removalPolicy: RemovalPolicy.DESTROY
     });
 
+    // Terraform cost SNS topic
+    const terraformCostTopic = new sns.Topic(this, 'TerraformCostTopic');
+
     // IAM permissions for CodeBuild
-    const terraformS3IAMPolicyForCodeBuild = new iam.ManagedPolicy(this, 'ManagedPolicy', {
+    const terraformS3IAMPolicyForCodeBuild = new iam.ManagedPolicy(this, 'S3CodeBuildManagedPolicy', {
       statements: [
         new iam.PolicyStatement({
-        actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
-        resources: [terraformStateBucket.arnForObjects('*')]
+          actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
+          resources: [terraformStateBucket.arnForObjects('*')]
         }),
         new iam.PolicyStatement({
           actions: ['s3:ListBucket'],
@@ -37,10 +43,18 @@ export class InfracostCdkPipelineStack extends Stack {
         })
       ]
     });
-    const terraformS3IAMRoleForCodeBuild = new iam.Role(this, 'Role', {
+    const terraformSNSIAMPolicyForCodeBuild = new iam.ManagedPolicy(this, 'SNSCodeBuildManagedPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['sns:Publish'],
+          resources: [terraformCostTopic.topicArn]
+        })
+      ]
+    });
+    const terraformPullRequestCodeBuildRole = new iam.Role(this, 'Role', {
       assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
       description: 'IAM role for CodeBuild to interact with S3',
-      managedPolicies: [terraformS3IAMPolicyForCodeBuild, iam.ManagedPolicy.fromAwsManagedPolicyName('AWSCodeCommitReadOnly')]
+      managedPolicies: [terraformS3IAMPolicyForCodeBuild, terraformSNSIAMPolicyForCodeBuild, iam.ManagedPolicy.fromAwsManagedPolicyName('AWSCodeCommitReadOnly')]
     });
 
     // Terraform source code
@@ -56,21 +70,24 @@ export class InfracostCdkPipelineStack extends Stack {
         phases: {
           install: {
             commands: [
+              'git checkout $CODEBUILD_SOURCE_VERSION',
+              'sudo yum -y install unzip python3-pip jq',
+              'sudo pip3 install git-remote-codecommit',
               `wget https://releases.hashicorp.com/terraform/${terraformVersion}/terraform_${terraformVersion}_linux_amd64.zip`,
-              'sudo yum -y install unzip python3-pip',
               `unzip terraform_${terraformVersion}_linux_amd64.zip`,
               'sudo mv terraform /usr/local/bin/',
               'curl -fsSL https://raw.githubusercontent.com/infracost/infracost/master/scripts/install.sh | sh',
-              'sudo pip3 install git-remote-codecommit',
               `git clone ${terraformRepository.repositoryCloneUrlGrc} --branch=${mainBranchName} --single-branch /tmp/main`,
-              'infracost breakdown --path /tmp/main --usage-file infracost-usage.yml --format json --out-file infracost-base.json'
+              'infracost breakdown --path /tmp/main --usage-file infracost-usage.yml --format json --out-file infracost-main.json'
             ]
           },
           build: {
             commands:[
               `terraform init -backend-config="bucket=${terraformStateBucket.bucketName}"`,
               'terraform plan',
-              'infracost diff --path . --compare-to infracost-base.json --usage-file infracost-usage.yml'
+              'infracost diff --path . --compare-to infracost-main.json --usage-file infracost-usage.yml --format json --out-file infracost-pull-request.json',
+              `DIFF_TOTAL_MONTHLY_COST=$(jq '.diffTotalMonthlyCost | tonumber | floor' infracost-pull-request.json)`,
+              `if [[ $DIFF_TOTAL_MONTHLY_COST -gt 0 ]]; then aws sns publish --topic-arn ${terraformCostTopic.topicArn} --message file://infracost-pull-request.json; fi`
             ]
           }
         }
@@ -88,10 +105,14 @@ export class InfracostCdkPipelineStack extends Stack {
         },
         privileged: true
       },
-      role: terraformS3IAMRoleForCodeBuild
+      role: terraformPullRequestCodeBuildRole
     });
     const pullRequestStateChangeRule = terraformRepository.onPullRequestStateChange('TerraformRepositoryOnPullRequestStateChange', {
-      target: new targets.CodeBuildProject(pullRequestCodeBuildProject),
+      target: new targets.CodeBuildProject(pullRequestCodeBuildProject, {
+        event: events.RuleTargetInput.fromObject({
+          sourceVersion: events.EventField.fromPath('$.detail.sourceReference'),
+        }),
+      })
     });
 
     // pipeline
@@ -126,7 +147,7 @@ export class InfracostCdkPipelineStack extends Stack {
         },
         privileged: true
       },
-      role: terraformS3IAMRoleForCodeBuild
+      role: terraformPullRequestCodeBuildRole
     });
     const terraformPipeline = new codepipeline.Pipeline(this, 'TerraformPipeline', {
       pipelineName: 'TerraformPipeline'
